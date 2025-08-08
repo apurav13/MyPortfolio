deb19d6dd0af8cfd1cfce990d41445fbb1c07a4b
apurav13 <apswami1305@gmail.com>
2025-08-08 11:54:33 +0530

    Initial clean commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import Tilt from 'react-parallax-tilt';
+import profileImage from '../../assets/profile2.jpg';
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side */}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* Greeting */}
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
+          {/* Name */}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-red-500 mb-4 leading-tight">
+            Apurav Swami
+          </h2>
+          {/* Skills Heading with Typing Effect */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-red-500 leading-tight">
+            <span className="text-white">I'm a </span>
+            <ReactTypingEffect
+             text={[
+  'Cybersecurity Enthusiast',
+  'Web Developer',
+  'Malware Analyst',
+  'Coder',
+  'Network Security Learner',
+  'CTF Challenger Creator',
+]}
+
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => (
+                <span className="text-red-500">{cursor}</span>
+              )}
+            />
+          </h3>
+          {/* Resume Button */}
+          <a
+            href="https://drive.google.com/file/d/1h-Ygj4zcb6wWZMN3Lnb3qM9T1_12mMWc/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: 'linear-gradient(90deg, #e51313ff, #eb1717ff)',
+              boxShadow: '0 0 2px #eb1717ff, 0 0 2px #eb1717ff, 0 0 40px #eb1717ff',
+            }}
+          >
+            DOWNLOAD CV
+          </a>
+
+          
+        </div>
+        {/* Right Side */}
+        <div className="md:w-1/2 flex justify-center md:justify-end">
+          <Tilt
+            className="w-45 h-68 sm:w-64 sm:h-64 md:w-[25rem] md:h-[25rem] border-4 border-red-700 rounded-full"
+            tiltMaxAngleX={30}
+            tiltMaxAngleY={35}
+            perspective={1000}
+            scale={1.005}
+            transitionSpeed={10000}
+            gyroscope={true}
+          >
+            <img
+              src={profileImage}
+              alt="Apurav Swami"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+            />
+          </Tilt>
+        </div>
+      </div>
+      
+      {/* About Me Paragraph */}
+          <p className="text-base sm:text-lg md:text-lg text-gray-500 mb-10 mt-8 leading-relaxed">
+            I'm a web developer and cybersecurity enthusiast with experience in building responsive 
+            applications and secure digital solutions. Skilled in HTML, CSS, Java, and C, I’ve developed 
+            projects like personal portfolios and the Alcohol Sense Engine Lock system, combining development 
+            with a strong security mindset.
+
+          </p>
+                
+    </section>
+  );
+};
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,308 @@
+// Skills Section Logo's
+
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+import GbnLogo from './assets/education_logo/GBN_logo.jpg';
+
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      //{ name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      //{ name: 'Angular', logo: angularLogo },
+      //{ name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      //{ name: 'Tailwind CSS', logo: tailwindcssLogo },
+      //{ name: 'GSAP', logo: gsapLogo },
+      //{ name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  
+    {
+  title: 'Cybersecurity',
+  skills: [
+    { name: 'Kali Linux', logo: 'https://www.bing.com/images/search?view=detailV2&ccid=5nOm%2fX%2bV&id=4CAAE3753CB853BA57FC2AD8B0CE012C95FB3A3D&thid=OIP.5nOm_X-VUwjma1bmCDy9GAHaHZ&mediaurl=https%3a%2f%2fcdn.freelogovectors.net%2fwp-content%2fuploads%2f2021%2f12%2fkali-logo-freelogovectors.net_.png&exph=1498&expw=1500&q=kali+linux+logo&simid=607989455578271972&FORM=IRPRST&ck=0D33C0B37CD9CCD7170808E8ABE25869&selectedIndex=0&itb=1&idpp=overlayview&ajaxhist=0&ajaxserp=0' },
+
+   // { name: 'Kali Linux', logo: kaliLogo },
+    //{ name: 'Wireshark', logo: wiresharkLogo },
+    //{ name: 'Burp Suite', logo: burpsuiteLogo },
+    //{ name: 'Metasploit', logo: metasploitLogo },
+    //{ name: 'Nmap', logo: nmapLogo },
+    //{ name: 'CTF Challenge Design', logo: ctfLogo },
+    //{ name: 'Phishing Simulation', logo: phishingLogo },
+  ],
+},
+
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      //{ name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      //{ name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      //{ name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+     // { name: 'Compass', logo: mcLogo },
+      //{ name: 'Wireshark', logo: WiresharkLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    //{
+    //   id: 0,
+    //   img: webverseLogo,
+    //   role: "Fullstack Developer",
+    //   company: "Webverse Digital",
+    //   date: "April 2024 - Present",
+    //   desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "JavaScript",
+    //     "React JS",
+    //     "TypeScript",
+    //     "Node JS",
+    //     "Tailwind CSS",
+    //     "MongoDb",
+    //     "Redux",
+    //     " Next Js",
+    //   ],
+    // },
+    // {
+    //   id: 1,
+    //   img: agcLogo,
+    //   role: "Fullstack Engineer",
+    //   company: "Agumentik Group of Companies",
+    //   date: "July 2023 - March 2024",
+    //   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    //   skills: [
+    //     "ReactJS",
+    //     "Redux",
+    //     "JavaScript",
+    //     "Tailwind CSS",
+    //     "HTML",
+    //     "CSS",
+    //     "SQL",
+    //   ],
+    // },
+    // {
+    //   id: 2,
+    //   img: newtonschoolLogo,
+    //   role: "Frontend Intern",
+    //   company: "Newton School",
+    //   date: "September 2021 - August 2022",
+    //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "Javascript",
+    //     "Bootstrap",
+    //     "Figma",
+    //     "Material UI",
+    //   ],
+    // },
+  ];
+  
+  export const education = [
+    // {
+    //   id: 0,
+    //   img: glaLogo,
+    //   school: "Manav Rachna University, Faridabad",
+    //   date: "Aug 2022 - June 2026",
+    //   grade: "9.1 CGPA",
+    //   desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
+    //   degree: "Master of Computer Applications - MCA",
+    // },
+    {
+      id: 0,
+      img: 'https://institute.careerguide.com/wp-content/uploads/2024/01/Manav-Rachna-University-1024x415.jpg',
+      school: "Manav Rachna University, Faridabad",
+      date: "Aug 2022 - June 2026",
+      grade: "9.1CGPA",  
+      desc: "I'm currently pursuing a B.Tech in Computer Science and Engineering with a specialization in Cyber Security and Threat Intelligence from Manav Rachna University. My academic journey has equipped me with strong fundamentals in programming, network security, threat analysis, and secure system design. With a CGPA of 9.03, I’ve also worked on projects like the Alcohol Sense Engine Lock and actively contributed to CTF and research-based cybersecurity initiatives.",
+      degree: "B.Tech CSE (Cyber Security & Threat Intelligence)",
+},
+    {
+      id: 1,
+      img: 'https://images.uniapply.com/uploads/college/image/logo/2579/Vidya_Mandir_Public_School_1013_Logo_1.jpg',
+      school: "Vidhya Mandir Public School, Faridabad",
+      date: "Apr 2021 - March 2022",
+      grade: "83.8%",
+      desc: "I completed my class 12 education from Vidhya Mandir Public School, Faridabad, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "CBSE(XII) - PCM with Computer Science",
+    },
+    {
+      id: 2,
+      img: 'https://images.uniapply.com/uploads/college/image/logo/2579/GBN_Senior_Secondary_School_909_Logo_1.jpg',
+      school: "GBN School, Faridabad",
+      date: "Apr 2019 - March 2020",
+      grade: "74.2%",
+      desc: "I completed my class 10 education from GBN School, Faridabad, under the CBSE board, where I studied Science with Computer.",
+      degree: "CBSE(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "GitHub Profile Detective",
+      description:
+        "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+      image: githubdetLogo,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+      github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+      webapp: "https://githubprofiledetective.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "CS Prep",
+      description:
+        "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+      image: csprepLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/codingmastr/CSPrep",
+      webapp: "https://csprep.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "Movie Recommendation App",
+      description:
+        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/codingmastr/Movie-Recommendation-App",
+      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Email Validator NPM Package",
+      description:
+        "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+      image: npmLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation"],
+      github: "https://github.com/codingmastr/cmtk-email-validator",
+      webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    },
+    {
+      id: 4,
+      title: "Task Reminder Chrome Extension Tool",
+      description:
+        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+      image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      github: "https://github.com/codingmastr/Task-Reminder-Tool",
+      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    },
+    {
+      id: 5,
+      title: "Webverse Digital",
+      description:
+        "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+      image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+      github: "https://github.com/codingmastr/Webverse-Digital",
+      webapp: "https://webversedigital.com/",
+    },
+    {
+      id: 6,
+      title: "Coding Master",
+      description:
+        "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+      image: cmLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+      github: "https://codingmasterweb.in/",
+      webapp: "https://codingmasterweb.in/",
+    },
+    {
+      id: 7,
+      title: "Image Search App",
+      description:
+        "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+      image: imagesearchLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Search-App",
+      webapp: "https://imagsearch.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "Image Background Remover",
+      description:
+        "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+      image: removebgLogo,
+      tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Background-Remover",
+      webapp: "https://removeyourbg.netlify.app/",
+    },
+  ];  
