00813487e8a41013d192e09dec5154167fcfe3c2
apurav13 <apswami1305@gmail.com>
2025-09-02 19:09:39 +0530

    fix: move timeline containers away from center line

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           </h3>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1hsbHIhOaFeran0iw2OSr1cMh-Zd6aHGG/view?usp=sharing"
+            href="https://drive.google.com/file/d/19b9H71LMPvaCKyufKY2MQhucSsMGL5yv/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
